72d1968b28fa3324e2e898111782af34b24d3a5f
Muhammad Adrees <madreesgujjar601@gmail.com>
2026-09-15 15:09:44 +0000

    fix out all

--- src/data/ImagesData.ts
+++ src/data/ImagesData.ts
@@ -111,4 +111,12 @@ const beforeAfterData: DataInterface[] = [
   },
 ];
 
-export { beforeAfterData };
+const resurfacingData = beforeAfterData.filter(
+  ({ category }) => category === "Walk-In Shower" || category === "Bathtub & Tile",
+);
+
+const microneedlingData = beforeAfterData.filter(
+  ({ category }) => category === "Tub-to-Shower",
+);
+
+export { beforeAfterData, microneedlingData, resurfacingData };
